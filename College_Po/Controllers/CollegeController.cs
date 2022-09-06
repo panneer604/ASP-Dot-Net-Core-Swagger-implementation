779a97d0c080b7ff2e18aceb4d2a69c3eb12f96c
panneer604 <112933233+panneer604@users.noreply.github.com>
2022-09-06 15:54:58 +0530

    Add files via upload

---
+++ College_Po/Controllers/CollegeController.cs
@@ -0,0 +1,93 @@
+﻿using College_Po.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace College_Po.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CollegeController : ControllerBase
+    {
+        private List<College> Collegeouss = new List<College>() {
+
+              new College(){
+
+                Id = 1,
+                Name = "Yasar Arafath",
+                Place = "Pondicherry",
+                District = "Namakkal"
+              }
+        };
+
+        [HttpGet]
+        [Route("GetCollege")]
+        public async Task<ActionResult<College>> GetCollege()
+        {
+
+            return Ok(Collegeouss);
+        }
+
+        [HttpGet]
+        [Route("GetCollegeById")]
+        public async Task<ActionResult<College>> GetCollege(int Id)
+
+        {
+
+            var college = Collegeouss.Find(val => val.Id == Id);
+            if (college == null)
+            {
+                return BadRequest("COLLEGE STUDENTS ARE NOT FOUND");
+            }
+            else
+            {
+                return Ok(college);
+            }
+        }
+
+        [HttpPost]
+        [Route("Add_College")]
+        public async Task<ActionResult<College>> AddCollege(College request)
+        {
+            Collegeouss.Add(request);
+            return Ok(Collegeouss);
+        }
+
+        [HttpPost]
+
+        [Route("Update College")]
+        public async Task<ActionResult<College>> UpdateCollege(College request)
+        {
+
+            var college = Collegeouss.Find(val => val.Id == request.Id);
+            if (college == null)
+            {
+                return BadRequest("Cannot able to Update the Employee");
+            }
+            else
+            {
+                college.Name = request.Name;
+                college.Place = request.Place;
+                college.District = request.District;
+            }
+            return Ok(Collegeouss);
+        }
+
+        [HttpDelete]
+
+        [Route("delete College")]
+        public async Task<ActionResult<College>> DeleteCollege(int Id)
+        {
+            var college = Collegeouss.Find(val => val.Id == Id);
+            if (college == null)
+            {
+                return BadRequest("Record Not Deleted");
+            }
+            else
+            {
+
+                Collegeouss.Remove(college);
+                return Ok(Collegeouss);
+            }
+        }
+    }
+}
---
+++ College_Po/Models/College.cs
@@ -0,0 +1,19 @@
+﻿namespace College_Po.Models
+{
+    public class College
+    {
+
+        public int Id { get; set; }
+
+        public String ? Name { get; set; }
+
+        public String ? Place{ get; set; }
+
+        public String ? District { get; set; }
+
+        internal static object Find(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
